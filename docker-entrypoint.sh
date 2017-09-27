e21b6dcc4b14d0ec2292e4e61dc67bda4fe31c1b
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-09-27 12:15:01 +0300

    precompilse assets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,13 @@
 set -e
 
 if [ "$1" = 'update' ]; then
-  bundle exec hanami db create
   bundle exec hanami db migrate
   exit 0
 fi
 
 if [ "$1" = 'web' ]; then
+  bundle exec hanami db migrate
+  bundle exec hanami assets precompile
   bundle exec puma -C config/puma.rb --bind tcp://0.0.0.0:80
 fi
 
